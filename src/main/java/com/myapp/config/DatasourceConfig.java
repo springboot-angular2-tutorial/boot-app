c55cf3ee0d8cf30f9f56067726e45027adb523e5
Akira Sosa <akirasosa@gmail.com>
2015-11-15 12:29:45 +0700

    fix local detasource config

--- src/main/java/com/myapp/config/DatasourceConfig.java
+++ src/main/java/com/myapp/config/DatasourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
@@ -32,6 +33,7 @@ public class DatasourceConfig {
     }
 
     @Bean
+    @Primary
     DataSource dataSource() {
         return new DataSourceSpy(realDataSource());
     }
