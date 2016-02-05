aa8d1a8d1d242a791ab521b3ff81462a94878506
Akira Sosa <akirasosa@gmail.com>
2016-02-05 17:45:22 +0700

    ...

--- src/main/java/com/myapp/repository/MicropostRepositoryImpl.java
+++ src/main/java/com/myapp/repository/MicropostRepositoryImpl.java
@@ -3,9 +3,9 @@ package com.myapp.repository;
 import com.myapp.domain.*;
 import com.myapp.dto.PostDTO;
 import com.myapp.dto.UserStats;
+import com.myapp.repository.helper.UserStatsQueryHelper;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ConstructorExpression;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -37,43 +37,17 @@ public class MicropostRepositoryImpl implements MicropostRepositoryCustom {
                                     Optional<Long> maxId,
                                     Integer maxSize) {
         final QMicropost qMicropost = QMicropost.micropost;
-        final QUser qUser = QUser.user;
         final QRelationship qRelationship = QRelationship.relationship;
 
-        final JPQLQuery<Relationship> relationshipSubQuery = JPAExpressions.selectFrom(qRelationship)
+        final ConstructorExpression<UserStats> userStatsExpression =
+                UserStatsQueryHelper.userStatsExpression(qMicropost.user, user);
+        final JPQLQuery<Relationship> relationshipSubQuery = JPAExpressions
+                .selectFrom(qRelationship)
                 .where(qRelationship.follower.eq(user)
                         .and(qRelationship.followed.eq(qMicropost.user))
                 );
-
-        final QMicropost qMicropost2 = new QMicropost("micropost1");
-        final JPQLQuery<Long> cntPostsQuery = JPAExpressions.select(qMicropost2.count())
-                .from(qMicropost2)
-                .where(qMicropost2.user.eq(qMicropost.user));
-
-        final QRelationship qRelationship1 = new QRelationship("relationship1");
-        final JPQLQuery<Long> cntFollowersQuery = JPAExpressions.select(qRelationship1.count())
-                .from(qRelationship1)
-                .where(qRelationship1.followed.eq(qMicropost.user));
-
-        final QRelationship qRelationship2 = new QRelationship("relationship2");
-        final JPQLQuery<Long> cntFollowingsQuery = JPAExpressions.select(qRelationship2.count())
-                .from(qRelationship2)
-                .where(qRelationship2.follower.eq(qMicropost.user));
-
-        final QRelationship qRelationship3 = new QRelationship("relationship3");
-        final JPQLQuery<Long> isFollowedByMeQuery = JPAExpressions.select(qRelationship3.count())
-                .from(qRelationship3)
-                .where(qRelationship3.followed.eq(qMicropost.user)
-                        .and(qRelationship3.follower.eq(user)));
-
-        final ConstructorExpression<UserStats> expression = Projections.constructor(UserStats.class,
-                cntPostsQuery,
-                cntFollowingsQuery,
-                cntFollowersQuery,
-                isFollowedByMeQuery
-        );
         final List<Tuple> intermediatePosts = queryFactory
-                .select(qMicropost, qMicropost.user, expression)
+                .select(qMicropost, qMicropost.user, userStatsExpression)
                 .from(qMicropost)
                 .innerJoin(qMicropost.user)
                 .where((qMicropost.user.eq(user).or(relationshipSubQuery.exists()))
@@ -89,7 +63,7 @@ public class MicropostRepositoryImpl implements MicropostRepositoryCustom {
             PostDTO post = PostDTO.builder()
                     .micropost(row.get(qMicropost))
                     .user(row.get(qMicropost.user))
-                    .userStats(row.get(expression))
+                    .userStats(row.get(userStatsExpression))
                     .build();
             posts.add(post);
         }
---
+++ src/main/java/com/myapp/repository/helper/UserStatsQueryHelper.java
@@ -0,0 +1,52 @@
+package com.myapp.repository.helper;
+
+import com.myapp.domain.QMicropost;
+import com.myapp.domain.QRelationship;
+import com.myapp.domain.QUser;
+import com.myapp.domain.User;
+import com.myapp.dto.UserStats;
+import com.querydsl.core.types.ConstructorExpression;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
+
+public class UserStatsQueryHelper {
+
+    public static ConstructorExpression<UserStats> userStatsExpression(QUser qUser, User currentUser) {
+        return Projections.constructor(UserStats.class,
+                cntPostsQuery(qUser),
+                cntFollowingsQuery(qUser),
+                cntFollowersQuery(qUser),
+                isFollowedByMeQuery(qUser, currentUser)
+        );
+    }
+
+    private static JPQLQuery<Long> isFollowedByMeQuery(QUser qUser, User currentUser) {
+        final QRelationship qRelationship = new QRelationship("relationship_is_followed_by_me");
+        return JPAExpressions.select(qRelationship.count())
+                .from(qRelationship)
+                .where(qRelationship.followed.eq(qUser)
+                        .and(qRelationship.follower.eq(currentUser)));
+    }
+
+    private static JPQLQuery<Long> cntFollowersQuery(QUser qUser) {
+        final QRelationship qRelationship = new QRelationship("relationship_cnt_followers");
+        return JPAExpressions.select(qRelationship.count())
+                .from(qRelationship)
+                .where(qRelationship.followed.eq(qUser));
+    }
+
+    private static JPQLQuery<Long> cntFollowingsQuery(QUser qUser) {
+        final QRelationship qRelationship = new QRelationship("relationship_cnt_followings");
+        return JPAExpressions.select(qRelationship.count())
+                .from(qRelationship)
+                .where(qRelationship.follower.eq(qUser));
+    }
+
+    private static JPQLQuery<Long> cntPostsQuery(QUser qUser) {
+        final QMicropost qMicropost = new QMicropost("micropost_cnt_posts");
+        return JPAExpressions.select(qMicropost.count())
+                .from(qMicropost)
+                .where(qMicropost.user.eq(qUser));
+    }
+}
