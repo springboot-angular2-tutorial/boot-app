56f43271dd017c797d614c17cbc0ddd297980a53
Akira Sosa <akirasosa@gmail.com>
2016-02-06 15:43:20 +0700

    removed unused code

--- src/main/java/com/myapp/repository/MicropostRepository.java
+++ src/main/java/com/myapp/repository/MicropostRepository.java
@@ -1,13 +1,7 @@
 package com.myapp.repository;
 
 import com.myapp.domain.Micropost;
-import com.myapp.domain.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MicropostRepository extends JpaRepository<Micropost, Long>, MicropostRepositoryCustom {
-
-    Integer countByUser(User user);
-
 }
--- src/main/java/com/myapp/repository/RelationshipRepository.java
+++ src/main/java/com/myapp/repository/RelationshipRepository.java
@@ -1,15 +1,11 @@
 package com.myapp.repository;
 
-import com.myapp.domain.User;
 import com.myapp.domain.Relationship;
+import com.myapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RelationshipRepository extends JpaRepository<Relationship, Long> {
     Optional<Relationship> findOneByFollowerAndFollowed(User follower, User followed);
-
-    Integer countByFollower(User user);
-
-    Integer countByFollowed(User user);
 }
