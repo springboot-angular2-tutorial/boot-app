c24f767a3486dae4e435c0fdea7de913ac6239f8
Akira Sosa <akirasosa@gmail.com>
2016-07-31 12:34:25 +0700

    revert baserepositorytest

---
+++ src/test/groovy/com/myapp/repository/BaseRepositoryTest.groovy
@@ -0,0 +1,14 @@
+package com.myapp.repository
+
+import com.myapp.config.DatasourceConfig
+import com.myapp.config.QueryDSLConfig
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.transaction.annotation.Transactional
+import spock.lang.Specification
+
+@Transactional
+@ActiveProfiles("test")
+@ContextConfiguration(classes = [RepositoryTestConfig, DatasourceConfig, QueryDSLConfig])
+abstract class BaseRepositoryTest extends Specification {
+}
