94651d2d03c62f30788f8af42d38792fb5e2087e
Akira Sosa <akirasosa@gmail.com>
2016-08-05 14:59:38 +0700

    Replace deprecated class

--- src/test/groovy/com/myapp/repository/RepositoryTestConfig.groovy
+++ src/test/groovy/com/myapp/repository/RepositoryTestConfig.groovy
@@ -1,8 +1,8 @@
 package com.myapp.repository
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
-import org.springframework.boot.orm.jpa.EntityScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
