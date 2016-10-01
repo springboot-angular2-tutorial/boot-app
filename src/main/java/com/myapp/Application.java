e66515612f296686014f24141e9f1470ed224ee0
Akira Sosa <akirasosa@gmail.com>
2016-10-01 21:11:34 +0700

    remove useless SpringBootServletInitializer

--- src/main/java/com/myapp/Application.java
+++ src/main/java/com/myapp/Application.java
@@ -4,10 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     @SuppressWarnings("unused")
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
