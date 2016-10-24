b76d7014ba7f023347f925f13f743d3c390a4955
Akira Sosa <akirasosa@gmail.com>
2016-10-24 11:58:36 +0700

    update userservicetest

--- src/test/groovy/com/myapp/service/UserServiceTest.groovy
+++ src/test/groovy/com/myapp/service/UserServiceTest.groovy
@@ -1,18 +1,23 @@
 package com.myapp.service
 
-import com.myapp.domain.Relationship
 import com.myapp.domain.User
-import com.myapp.dto.PageParams
 import com.myapp.dto.UserDTO
+import com.myapp.dto.UserParams
 import com.myapp.repository.RelationshipRepository
+import com.myapp.repository.UserCustomRepository
 import com.myapp.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 
 @SuppressWarnings("GroovyPointlessBoolean")
 class UserServiceTest extends BaseServiceTest {
 
     @Autowired
     UserRepository userRepository
+
+    @Autowired
+    UserCustomRepository userCustomRepository
 
     @Autowired
     RelationshipRepository relationshipRepository
@@ -22,10 +27,10 @@ class UserServiceTest extends BaseServiceTest {
     UserService userService
 
     def setup() {
-        userService = new UserServiceImpl(userRepository, userDTORepository, securityContextService)
+        userService = new UserServiceImpl(userRepository, userCustomRepository, securityContextService)
     }
 
-    def "can find a user when not signed in"() {
+    def "findOne"() {
         given:
         User user = userRepository.save(new User(username: "akira@test.com", password: "secret", name: "akira"))
 
@@ -33,26 +38,57 @@ class UserServiceTest extends BaseServiceTest {
         UserDTO userDTO = userService.findOne(user.id).get()
 
         then:
-        userDTO.isMyself == null
+        userDTO.id == user.id
     }
 
-    def "can find a user when signed in"() {
+    def "findMe"() {
         given:
         User user = userRepository.save(new User(username: "akira@test.com", password: "secret", name: "akira"))
         securityContextService.currentUser() >> user
 
         when:
-        UserDTO userDTO = userService.findOne(user.id).get()
+        UserDTO userDTO = userService.findMe().get()
 
         then:
-        userDTO.isMyself == true
+        userDTO.id == user.id
+    }
+
+    def "update"() {
+        given:
+        User user = userRepository.save(new User(username: "akira@test.com", password: "secret", name: "akira"))
 
         when:
-        User anotherUser = userRepository.save(new User(username: "another@test.com", password: "secret", name: "another"))
-        userDTO = userService.findOne(anotherUser.id).get()
+        UserParams params = new UserParams("test2@test.com", "secret2", "test2")
+        userService.update(user, params)
 
         then:
-        userDTO.isMyself == false
+        user.username == "test2@test.com"
+        user.name == "test2"
+
+        when:
+        params = new UserParams("test3@test.com", null, null)
+        userService.update(user, params)
+
+        then:
+        user.username == "test3@test.com"
+        user.name == "test2"
+    }
+
+    def "loadUserByUsername"() {
+        given:
+        User user = userRepository.save(new User(username: "akira@test.com", password: "secret", name: "akira"))
+
+        when:
+        UserDetails userDetails = userService.loadUserByUsername("akira@test.com")
+
+        then:
+        user.username == userDetails.username
+
+        when:
+        userService.loadUserByUsername("test1@test.com")
+
+        then:
+        thrown(UsernameNotFoundException)
     }
 
 }
