cd1423a7cc814120faf7a901bba8125e7bd5cdd6
Akira Sosa <akirasosa@gmail.com>
2016-11-02 13:36:04 +0700

    update sns message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,5 +13,5 @@ aws s3 cp ./target/springboot-angular2-tutorial-0.1.0.jar s3://deploy-${ENV}.han
 # Publish to SNS Topic
 account_number=$(aws sts get-caller-identity --output text --query 'Account')
 aws sns publish --topic-arn "arn:aws:sns:${AWS_DEFAULT_REGION}:${account_number}:backend_app_updated" \
-   --message "{\"asgName\": \"web\"}"
+   --message "${ENV}: ${TRAVIS_COMMIT}"
 
